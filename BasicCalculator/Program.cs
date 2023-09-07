454cf89c2257f8552f450910626ffb8a467ce416
Pakkoyan <maksimenkobogdan12@gmail.com>
2023-09-07 10:32:11 +0600

    FirstAndLast

---
+++ BasicCalculator/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, from calculator!!");
+Console.WriteLine("Впишите 2 числа");
+
+int firstNum = int.Parse(Console.ReadLine());
+int secondNum = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Результат сложения равен {0}", firstNum + secondNum);
